47d6002f8f13935ff62d71adc21828f371b1eb67
Lukas Marx <marxlukas@googlemail.com>
2019-04-26 08:29:05 +0200

    feat(electron): updated to version 5 fix(nodeIntegration): enabled node integration

--- electron/main.ts
+++ electron/main.ts
@@ -14,7 +14,11 @@ app.on("activate", () => {
 });
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true }
+  });
 
   win.loadURL(
     url.format({
